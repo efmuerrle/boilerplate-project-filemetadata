6a6d330756862a32defee41d60588bc7a2696f8b
Emil Félix Muerrle <gitcommit@chaiya.es>
2019-11-14 21:49:12 +0100

    Fixes & Prettier formatting

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-"use strict";
+'use strict';
 
-var express = require("express");
-var cors = require("cors");
+var express = require('express');
+var cors = require('cors');
 
-// require and use "multer"...
-var multer = require("multer");
+var multer = require('multer');
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 
 var app = express();
 
 app.use(cors());
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get("/", function(req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get("/hello", function(req, res) {
-  res.json({ greetings: "Hello, API" });
+app.get('/hello', function(req, res) {
+  res.json({ greetings: 'Hello, API' });
 });
 
-app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
   const file = req.file;
   // console.log('file', file);
   const result = {
